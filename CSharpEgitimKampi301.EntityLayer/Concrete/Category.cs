4d5b98ec3c41eaa71adae171a02a62c227a4666c
m.sinans <msomercikoglu@gmail.com>
2024-11-06 10:44:09 +0300

    12. Ders

--- CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -11,5 +11,6 @@ namespace CSharpEgitimKampi301.EntityLayer.Concrete
         public int CategoryId { get; set; } // Code Firt de bunun Birincil anahtar ve otomatik artan olması için sınıf adı sonuna Id eklenerek bu property oluşturulmalı
         public string CategoryName { get; set; }
         public bool CategoryStatus { get; set; }
+        public List<Product> Products { get; set; } // Product tablosu ile ilişkisini oluşturuyorum
     }
 }
--- CSharpEgitimKampi301.EntityLayer/Concrete/Product.cs
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Product.cs
@@ -10,8 +10,13 @@ namespace CSharpEgitimKampi301.EntityLayer.Concrete
     {
         public int ProductId { get; set; }
         public string ProductName { get; set; }
-        public int productStock { get; set; }
-        public decimal productPrice { get; set; }
+        public int ProductStock { get; set; }
+        public decimal ProductPrice { get; set; }
         public string ProductDescription { get; set; }
+        public int CategoryId { get; set; } // Her bir ürünün kategorisi olmalı o yüzden bunu ekledim
+        public virtual Category Category { get; set; } // Bir ürünün kategori bilgisine ulaşmak için Category türünde bir property ekledim. Böylelikle Category tablosu ile ilişkilendirdim ama bu durumdan Category tablosunun da haberdar olması gerekiyor. Oraya gidip public List<Product> Products { get; set; } ekliyorum
+
+        public List<Order> Orders { get; set; }
+
     }
 }
