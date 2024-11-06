b3cfbd024811bb522a4d02020039650c5c838cfa
m.sinans <msomercikoglu@gmail.com>
2024-11-06 10:15:31 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category // bu sınıf veritabanında ilgili tablolara ait property leri tutuyor
+    {
+        public int CategoryId { get; set; } // Code Firt de bunun Birincil anahtar ve otomatik artan olması için sınıf adı sonuna Id eklenerek bu property oluşturulmalı
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+    }
+}
---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int productStock { get; set; }
+        public decimal productPrice { get; set; }
+        public string ProductDescription { get; set; }
+    }
+}
